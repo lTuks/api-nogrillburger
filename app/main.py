ded6dc91a1a0623f5152e0a5dfde2f8bc726d220
Vinícius Lopo <vsantos.lvieira@gmail.com>
2022-02-01 21:04:23 -0300

    alter db

--- app/main.py
+++ app/main.py
@@ -109,7 +109,9 @@ async def create_stock(stock: Stock, api_key: APIKey = Depends(get_api_key)):
 
 @noGrill.put("/stock/{id}")
 async def update_stock(id: str, stock: Stock, api_key: APIKey = Depends(get_api_key)):
-    update_stock = = await (
+    update_stock = await (
         db["stock"].update_one({"_id": id}, {"$set": stock.dict()}).raw_result
     )
+
+
 return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
